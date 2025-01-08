5df0ad5f75f0ae84a375b8f2c15fb0afe7fcc3ad
Sergey Bespalov <sergey.bespalov@protonmail.com>
2025-01-08 17:58:46 +0300

    fileformat

--- init.lua
+++ init.lua
@@ -203,6 +203,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.opt.fileformats = 'unix,dos,mac'
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
